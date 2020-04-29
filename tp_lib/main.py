41ae4224d893a25e2c6cb9b0f91f8410ef690154
Frederic GAURAT <fgaurat@eolem.com>
2020-04-29 16:52:47 +0200

    mysql insert

--- tp_lib/main.py
+++ tp_lib/main.py
@@ -2,6 +2,7 @@ import argparse
 import configparser
 from pprint import pprint
 import mysql.connector
+import requests
 
 if __name__ == "__main__":
     try:
@@ -16,6 +17,23 @@ if __name__ == "__main__":
 
         cnx = mysql.connector.connect(**config['MySQL'])
         cursor = cnx.cursor()
+        
+        r = requests.get('http://ns349423.ip-94-23-40.eu:54413/todos')
+        data_json =r.json()
+
+        for d in data_json:
+            completed = 1 if d['completed'] == 'True' else 0
+            sql = ("INSERT INTO `paris-form-1200` (`title`, `done`, `due_date`) " 
+                    "VALUES ('{}', '{}', '{}')".format(d['title'],completed,int(d['dueDate']/1000)) )
+            print(sql)
+            cursor.execute(sql)
+        cnx.commit()
+
+
+    # completed == 'True'?1:0
+
+
+
         query = "SELECT * FROM `paris-form-1200`"
         cursor.execute(query)
         for todo in cursor:
